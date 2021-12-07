72eff94ef00ae186e1a4e992e9a636b7f21b587f
JHProjects <jan.holecek@post.cz>
2021-12-08 00:01:10 +0100

    dropdown menu and other minor features

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 let img = document.querySelector("img")
+let container = document.querySelector(".container")
+let bookName = document.querySelector("h2 span")
 img.addEventListener("click", ez)
+bookName.addEventListener("click", openContainer)
+
 
 function ez() {
-	if (!img.classList.contains("animation")) {img.classList.add("animation")}
-		else {img.classList.remove("animation")}
+	if (!img.classList.contains("animation")) {
+		img.classList.add("animation")
+		let timeOut = setTimeout(e => {
+			img.classList.remove("animation")
+		}, 10000)
+	} else {
+		clearTimeout("timeOut")
+		img.classList.remove("animation")
+	}
+}
+
+let data = {}
+function openContainer(e) {
+	if (!container.classList.contains("container-open")) {
+		container.classList.add("container-open")
+		data.name = e.target.textContent
+		console.log(e.target.textContent)
+		e.target.innerHTML = "hello from main.js"
+	} else {
+		container.classList.remove("container-open")
+		e.target.innerHTML = data.name
+	}
+	
+	
 }
